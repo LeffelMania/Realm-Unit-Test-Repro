b12e67123ea722a3620bf9b1b5fd3c4ac8b94b66
Alex Leffelman <alex@remind101.com>
2016-03-18 15:16:42 -0500

    Adding Realm directly to repo;

---
+++ Podfile
@@ -0,0 +1,7 @@
+target 'Realm-UnitTests', :exclusive => true do
+    pod 'Realm'
+end
+
+target 'Realm-UnitTestsTests', :exclusive => true do
+    pod 'Realm/Headers'
+end
---
+++ Pods/Target Support Files/Pods-Realm-UnitTestsTests/Pods-Realm-UnitTestsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Realm_UnitTestsTests : NSObject
+@end
+@implementation PodsDummy_Pods_Realm_UnitTestsTests
+@end
